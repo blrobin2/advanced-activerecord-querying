9bb9be1e8bce14eb52fdd5b77a6bc56474ef5f8d
Bennie Robinson <brobinson88@gmail.com>
2021-04-08 11:20:29 -0400

    Test in_region for person

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,7 @@ class Person < ApplicationRecord
   has_many :employees, class_name: 'Person', foreign_key: :manager_id
 
   scope :billable, -> { joins(:role).merge(Role.billable) }
-  scope :in_region, -> (region) { joins(:location).merge(Location.in_region(region)) }
+  scope :in_region, ->(region) { joins(:location).merge(Location.in_region(region)) }
 
   scope :alphabetically_by_region_and_location, -> { all }
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'in_region' do
+    result = Person.in_region('expected')
+    assert result.map(&:name).sort == %w[Christie Wendell]
+  end
 end
